ef2852e4588d292c02339a84f3ab900069e55c03
alexanderbjorlo <alexanderbjorlo@alexanders-macbook-pro.local>
2019-04-29 23:00:50 +0200

    close on filehandling

--- src/main/java/com/mikkelgud/person/PersonData.java
+++ src/main/java/com/mikkelgud/person/PersonData.java
@@ -13,7 +13,7 @@ public class PersonData {
     public String firstName;
     public String lastName;
     public String billingAddress;
-    
+
     public PersonData(
             String firstName,
             String lastName,
@@ -26,9 +26,47 @@ public class PersonData {
     
     @Override
     public String toString(){
-        String out = "First name: " + firstName + "Last name: " + lastName + "Billing address: " + billingAddress;
+        String out = "First name: " + firstName + "Last name: " + lastName + "Billing address: " + billingAddress + "\n";
         System.out.print(out);
-        return "First name: " + firstName + "Last name: " + lastName + "Billing address: " + billingAddress;
+        return "First name: " + firstName + "Last name: " + lastName + "Billing address: " + billingAddress +"\n";
     }
-    
+
+    public void setBillingAddress(String billingAddress) {
+        this.billingAddress = billingAddress;
+    }
+
+    public String getBillingAddress() {
+        return firstName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getLastName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void attatchToGUI(){
+
+        try {
+            PersonValidator validator = new PersonValidator();
+            Person newPerson = validator.createNew(getFirstName(), getLastName(), getBillingAddress());
+
+
+            PersonListModel personListModel = new PersonListModel();
+            personListModel.getPersonList().add(newPerson);
+
+        } catch (InvalidPersonPropertiesException ex) {
+
+        }
+    }
 }
