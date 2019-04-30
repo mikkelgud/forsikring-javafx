7bf10e16a65f4bdaeed9ba16683a10a1387a6ef4
mikkelgud <mikkel_gudmundsen@hotmail.com>
2019-04-30 16:37:04 +0200

    Just pushing

--- src/main/java/com/mikkelgud/person/PersonData.java
+++ src/main/java/com/mikkelgud/person/PersonData.java
@@ -1,14 +1,7 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package com.mikkelgud.person;
 
-/**
- *
- * @author alexanderbjorlo
- */
+
 public class PersonData {
     public String firstName;
     public String lastName;
