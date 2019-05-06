60fcebf874a89a89e297d54a8e13a6fa0af1f00d
mikkelgud <mikkel_gudmundsen@hotmail.com>
2019-05-06 15:03:45 +0200

    *Made method to tes booleanArray in insurance validator

--- src/main/java/com/mikkelgud/insurance/InsuranceValidator.java
+++ src/main/java/com/mikkelgud/insurance/InsuranceValidator.java
@@ -1,14 +1,15 @@
 package com.mikkelgud.insurance;
 
 public class InsuranceValidator {
-    //Only validating one thing so only one message i needed
     private static final String MESSAGE_MUST_REGISTRATE_A_USER = "Vennligst merk av en registrert person";
     private static final String MESSAGE_NO_EMPTY_FIELDS = "Venligst fyll ut alle feltetene i registreringen";
     private static final String MESSAGE_ONLY_EMPTY_FIELDS = "Vennligst Kryss av minst ett felt!";
     private static final String MESSAGE_LENGTH_NOT_A_NUMBER = "Vennligst påse at lengden er et helttall";
     private static final String MESSAGE_NOT_A_NUMBER = "Nummer må skrives inn som heltall";
+    private static final int NUMBER_OF_WORLD_PARTS = 6;
 
     public BoatInsurance createNewBoatInsurance(String currentPersonId, String insuranceYearlyPayment, String insuranceAmount, String insuranceInfo, String boatType, String length, String model, String engineType, String enginePower) throws InvalidInsurancePropertiesException {
+
         if (!isPresent(currentPersonId)) {
             throw new InvalidInsurancePropertiesException(MESSAGE_MUST_REGISTRATE_A_USER);
         }
@@ -51,33 +52,35 @@ public class InsuranceValidator {
         if (!isPresent(currentPersonID)) {
             throw new InvalidInsurancePropertiesException(MESSAGE_MUST_REGISTRATE_A_USER);
         }
-
         if (!isPresent(address)) {
-        throw new InvalidInsurancePropertiesException(MESSAGE_NO_EMPTY_FIELDS);
+            throw new InvalidInsurancePropertiesException(MESSAGE_NO_EMPTY_FIELDS);
     }
-
         if (!isNumber(cabinSize)) {
             throw new InvalidInsurancePropertiesException(MESSAGE_NOT_A_NUMBER);
     }
-
         if (!isNumber(yearBuilt)) {
             throw new InvalidInsurancePropertiesException(MESSAGE_NOT_A_NUMBER);
     }
-
         return new CabinInsurance(currentPersonID, insuranceYearlyPayment, insuranceAmount, insuranceCoverageInfo, address, buildingMaterial, housingState, cabinSize, buildingType, yearBuilt, insurancePriceBuilding, insurancePriceHouseholdGoods);
     }
 
     public TravelInsurance createNewTravelInsurance(String currentPersonId, String insuranceYearlyPayment, String insuranceAmount, String insuranceCoverage, boolean southAmerica, boolean northAmerica, boolean asia, boolean oseania, boolean africa, boolean europe) throws InvalidInsurancePropertiesException {
+        boolean[] travelInsuranceBooleans = new boolean[NUMBER_OF_WORLD_PARTS];
+        travelInsuranceBooleans[0] = europe;
+        travelInsuranceBooleans[1] = asia;
+        travelInsuranceBooleans[2] = northAmerica;
+        travelInsuranceBooleans[3] = southAmerica;
+        travelInsuranceBooleans[4] = oseania;
+        travelInsuranceBooleans[5] = africa;
 
         if (!isPresent(currentPersonId)) {
             throw new InvalidInsurancePropertiesException(MESSAGE_MUST_REGISTRATE_A_USER);
         }
-        if (southAmerica == false && northAmerica == false && asia == false && oseania == false && africa == false && europe == false) {
+        if (!isAtLeastOneMarked(travelInsuranceBooleans)) {
             throw new InvalidInsurancePropertiesException(MESSAGE_ONLY_EMPTY_FIELDS);
         }
         return new TravelInsurance(currentPersonId, insuranceYearlyPayment, insuranceAmount, insuranceCoverage, europe, asia, northAmerica, southAmerica, oseania, africa);
     }
-
 
     private boolean isPresent(String stringToValidate) {
         return stringToValidate != null && !stringToValidate.isEmpty();
@@ -90,5 +93,12 @@ public class InsuranceValidator {
         } catch (Exception ex) {
             return false;
         }
+    }
+
+    private boolean isAtLeastOneMarked(boolean[] booleansToValidate) {
+        for (boolean e : booleansToValidate) {
+            if (e == true) return true;
+        }
+        return false;
     }
 }
